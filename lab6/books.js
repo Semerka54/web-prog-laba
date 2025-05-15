c7a4bebd31957f940de5a7d7053ed941de2de6de
Богданов Семён <semerka.semen@yandex.ru>
2025-05-15 18:54:52 +0700

    Добавил JS и обновил lab6.html

---
+++ lab6/books.js
@@ -0,0 +1,83 @@
+const books = [
+    {
+      author: 'Лао-цзы',
+      name: 'Дао дэ цзин. Канон о Дао и дэ',
+      publicationYear: 2022,
+      price: 520,
+      bestseller: true,
+      available: true
+    },
+    {
+      author: 'Карл Саган',
+      name: 'Космос',
+      publicationYear: 2024,
+      price: 690,
+      bestseller: true,
+      available: false
+    },
+    {
+      author: 'Баладжи Касал',
+      name: 'Ошибки разумного инвестора',
+      publicationYear: 2025,
+      price: 740,
+      bestseller: false,
+      available: true
+    },
+    {
+      author: 'Фёдор Достоевский',
+      name: 'Преступление и наказание',
+      publicationYear: 1866,
+      price: 410,
+      bestseller: true,
+      available: true
+    },
+    {
+      author: 'Джордж Оруэлл',
+      name: '1984',
+      publicationYear: 1949,
+      price: 390,
+      bestseller: true,
+      available: false
+    },
+    {
+      author: 'Юваль Ной Харари',
+      name: 'Sapiens: Краткая история человечества',
+      publicationYear: 2011,
+      price: 800,
+      bestseller: true,
+      available: true
+    },
+    {
+      author: 'Айзек Азимов',
+      name: 'Основание',
+      publicationYear: 1951,
+      price: 550,
+      bestseller: false,
+      available: false
+    },
+    {
+      author: 'Рэй Брэдбери',
+      name: '451 градус по Фаренгейту',
+      publicationYear: 1953,
+      price: 430,
+      bestseller: true,
+      available: true
+    },
+    {
+      author: 'Нассим Талеб',
+      name: 'Чёрный лебедь',
+      publicationYear: 2007,
+      price: 620,
+      bestseller: true,
+      available: true
+    },
+    {
+      author: 'Виктор Франкл',
+      name: 'Сказать жизни «Да!»',
+      publicationYear: 1946,
+      price: 460,
+      bestseller: false,
+      available: true
+    }
+  ];
+  
